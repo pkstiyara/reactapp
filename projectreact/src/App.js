3ef8011085e4cc9e0d678073410d1c4db06ce490
pankajlht <95473121+pankajlht@users.noreply.github.com>
2022-08-27 15:37:13 +0530

    simple react

---
+++ projectreact/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"; 
+import Tweet from "./Tweet";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1> This is a test </h1>
+      < Tweet name="Pankaj"/>
+
+    </div>
+
+  )
+};
+export default App;
